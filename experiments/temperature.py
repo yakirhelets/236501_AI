a333407426faf72faa43a92c9abf36794d5a49a3
Gal Fleissig <fleissigal@gmail.com>
2018-11-28 09:17:57 +0200

    Finish Section G, q19 - beginning

--- experiments/temperature.py
+++ experiments/temperature.py
@@ -3,8 +3,33 @@ from matplotlib import pyplot as plt
 
 X = np.array([400, 450, 900, 390, 550])
 
-# TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+N = 5
+num_of_experiments = 100
+start_point = 0.01
+end_point = 5.0
+
+T = np.linspace(start_point, end_point, num_of_experiments, True, False, float)
+
+alpha = min(X)
+P = [[0 for x in range(X.size)] for y in range(T.size)]
+
+print(P)
+
+def calc_probability(T, xi, alpha, X):
+    numerator = (xi / alpha)**(-1 / T)
+    denominator = 0
+
+    for x in X:
+        denominator += (x / alpha)**(-1 / T)
+
+    return float(numerator / denominator)
+
+
+for i in range(len(X)):
+    xi = X[i]
+    for j in range(len(T)):
+        P[j][i] = calc_probability(T[j], xi, alpha, X)
+
 
 print(P)
 
@@ -18,3 +43,10 @@ plt.legend()
 plt.grid()
 plt.show()
 exit()
+
+
+###
+P = [[0 for x in range(5)] for y in range(100)]
+for i in range(5):
+    P[:, i]
+
